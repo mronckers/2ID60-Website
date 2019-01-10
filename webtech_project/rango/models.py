515729e197f1ef4801806f85d3c1ce9f18a92352
luciaasen <luciaasen@gmail.com>
2019-01-10 16:05:18 +0100

    db created

--- webtech_project/rango/models.py
+++ webtech_project/rango/models.py
@@ -3,3 +3,29 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField(max_length = 254, unique = True) 
+    name = models.CharField(max_length=128)
+    def __str__(self): 
+        return self.name
+    def __unicode__(self):
+        return self.name
+
+class TodoList(models.Model):
+    name = models.CharField(max_length = 128, unique = True)
+    colour = models.CharField(max_length = 128)
+    owner = models.ForeignKey(User)
+    def __str__(self): 
+        return self.name
+    def __unicode__(self):
+        return self.name
+  
+class Task(models.Model):
+    content = models.TextField()
+    parent_list = models.ForeignKey(TodoList)
+    def __str__(self):
+        return self.content
+    def __unicode__(self):
+        return self.content
+
+
