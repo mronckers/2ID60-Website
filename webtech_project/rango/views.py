fffbc72e37fea64c2070a563c30caa112ace9d43
mronckers <m.l.m.ronckers@student.tue.nl>
2019-01-12 12:35:10 +0100

    Use database in index html

--- webtech_project/rango/views.py
+++ webtech_project/rango/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import TodoList
 
 from django.http import HttpResponse
 def index(request):
-    context_dict = {'boldmessage': 'beautiful person'}
-    return render(request, 'rango/index.html', context = context_dict)
+    #context_dict = {'boldmessage': 'beautiful person'}
+    lists = TodoList.objects.all()
+    return render(request, 'rango/index.html',{'lists': lists}) #context = context_dict)
 def about(request):
     return HttpResponse("Something about page and <a href='/rango/'>link to index)</a>")
