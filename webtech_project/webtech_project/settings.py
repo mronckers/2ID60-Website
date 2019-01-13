a01d0fa883a04c10fd6dd597b10f2f21a9bad836
mronckers <m.l.m.ronckers@student.tue.nl>
2019-01-13 14:57:54 +0100

    change setting

--- webtech_project/webtech_project/settings.py
+++ webtech_project/webtech_project/settings.py
@@ -14,8 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-#STATIC_DIR = os.path.join(BASE_DIR, 'static')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -119,5 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 # STATICFILES_DIRS = [STATIC_DIR, ]
-STATIC_URL = '/static/'
+STATIC_URL = '/webtech_project/rango/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'rango/static')
